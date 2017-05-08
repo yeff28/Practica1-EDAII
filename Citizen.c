d4ab7a47340acf1b6399e0e23d9cd271be571fea
u140176 <u140176@54005p09.ds.upf.edu>
2017-05-08 11:45:39 +0200

    #3 -> mostrar ciudadano

--- Citizen.c
+++ Citizen.c
@@ -15,7 +15,11 @@ Citizen read_citizen()
 
 void print_citizen(Citizen c) 
 {
-    /**
-        Print Citizen data
-    **/
+    printf("DNI:%i\n",c.documento);
+    printf("Nombre:%s\n",c.nombre);
+    printf("Apellido:%s\n",c.apellido);
+    printf("Sexo:%c\n",c.sexo);
+    printf("Dia nacimiento:%i\n",c.fecha_nacimiento.dia);
+    printf("Mes nacimiento:%i\n",c.fecha_nacimiento.mes);
+    printf("Año nacimiento:%i\n",c.fecha_nacimiento.any);
 }
