c83f309cf1d54107362336fc8be950e254e6ce79
SandraRodri <sandra.rodriguez06@estudiant.upf.edu>
2017-05-09 14:26:32 +0200

    Update HashTable.c

--- HashTable.c
+++ HashTable.c
@@ -3,123 +3,121 @@
 #include "HashTable.h"
 #include "sort.h"
 
-int hash_function(int key) 
-{ 
-    return key % N;  // position
+int hash_function(int key) {
+    return key % N; // position
 }
 
-void init_table(HashTable* table) 
-{
+void init_table(HashTable* table) {
     int i;
-    for(i=0; i<N; i++){
-        table->list[i].start=0; 
+    for (i = 0; i < N; i++) {
+        table->list[i].start = 0;
     }
 }
 
-void clear_table(HashTable* table) 
-{
-    int i, key ;
+void clear_table(HashTable* table) {
+    int i, key;
 
-    for(i=0; i<N; i++){
-        if(table->list[i].start != NULL){
+    for (i = 0; i < N; i++) {
+        if (table->list[i].start != NULL) {
             Node* current = table->list[i].start;
-            while(current != 0){
-                table->list[i].start = current->next;
-                free(current);
-                current=table->list[i].start;
-            }
-        }
-        else{
+            clear_list(current);
+        } else {
             printf("En la posicion %d esta vacio\n", i);
         }
-    }   
+    }
 }
 
-Node* find_citizen_by_id(HashTable* table, int id) 
-{
+Node* find_citizen_by_id(HashTable* table, int id) {
     int key = hash_function(id);
-    if(table->list[key].start != NULL){
+    if (table->list[key].start != NULL) {
         Node* current = table->list[key].start;
-        while(current != 0){
-            if(current->ciudadanos.documento == id){
+        while (current != 0) {
+            if (current->ciudadanos.documento == id) {
                 return current;
             }
-            current=table->list[key].start;
+            current = table->list[key].start;
         }
     }
 }
 
-bool exists_citizen_with_id(HashTable* table, int id) 
-{
+bool exists_citizen_with_id(HashTable* table, int id) {
+
     /**
         Checks if a citizen with ID id exists in the hashTable. Task 2.
-    **/
+     **/
 }
 
-bool insert_citizen(HashTable* table, Citizen c) 
-{
+bool insert_citizen(HashTable* table, Citizen c) {
     int key = hash_function(c.documento);
-    Node* aux = (Node *) malloc (sizeof(Node));
-    aux->ciudadanos = c;
-    if(aux==NULL){
-        printf("No hay memoria disponible");
-    }else{       
-        if(table->list[key].start == 0){
-        table->list[key].start = aux;
-        return 1;
     
-        }else{
-        Node* current = table->list[key].start;
-        while(current != 0){
-            current = current->next;
-        } 
-        current->next = aux;
-        return 0;
+    Node* aux = (Node *) malloc(sizeof (Node));
+    if (aux == NULL) {
+        printf("No hay memoria disponible");
+    } else {
+        aux->ciudadanos = c;
+        int existe = exists_citizen_with_id(table, c.documento);
+        if(existe){
+            return false;
+        } else {
+            if (table->list[key].start == 0) {
+                table->list[key].start = aux;
+                return true;
+            } 
+            else {
+                Node* current = table->list[key].start;
+                while (current != 0) {
+                    current = current->next;
+                }
+                current->next = aux;
+                return true;
+            }
         }
     }
 }
 
-bool delete_citizen_by_id(HashTable* table, int id) 
-{
+bool delete_citizen_by_id(HashTable* table, int id) {
     int key = hash_function(id);
-    if(table->list[id].start != NULL){
+    if (table->list[id].start != NULL) {
         Node* current = table->list[key].start;
-        
-        while(current != 0){
-            if(current->ciudadanos.documento == id){
+
+        while (current != 0) {
+            if (current->ciudadanos.documento == id) {
                 free(current);
                 return 1;
             }
-            current=table->list[id].start;
+            current = table->list[id].start;
         }
-    }
-    else{
+    } else {
         return 0;
     }
 }
 
-bool insert_citizen_list(HashTable* table, LinkedList* list) 
+bool insert_citizen_list(HashTable* table, int list_size) 
 {
-    /**
-        Inserts a citizen list to the hash table. Task 3
-    **/
+    Citizen c;
+    while (list_size-- > 0){
+        c = read_citizen();
+        int insertado = insert_citizen(table, c);
+        if(insertado){
+            printf("Se ha introducido correctamente el ciudadano\n");
+        }
+        else{
+            printf("El usuario con el dni %d ya se ha introducido \n", c.documento);
+        }  
+    }
 }
 
-bool update_citizen_info(HashTable* table, Citizen new_info) 
-{
-   int b;
-   b = hash_function(new_info.documento );
-   Node* s = find_citizen_by_id(table, b); //position
-   if(s){
-       s->ciudadanos.documento = new_info.documento;
-       printf(" %d ", s->ciudadanos.documento);
-   }
-   else{
-       return 0;
-   }
-    
-}
-
+bool update_citizen_info(HashTable* table, Citizen new_info) {
+    int b;
+    b = hash_function(new_info.documento);
+    Node* s = find_citizen_by_id(table, b); //position
+    if (s) {
+        s->ciudadanos = new_info;
+        printf(" %d ", s->ciudadanos.documento);
+        return true;
+    } else {
+        return false;
+    }
 
 void print_sorted_citizens(HashTable* table) 
 {
