5b87071ce5304408b45c37b957423a9eacbdc8aa
U137651 <U137651@54006p12.ds.upf.edu>
2017-05-18 12:09:59 +0200

    control del documento

--- Citizen.c
+++ Citizen.c
@@ -7,11 +7,22 @@
 Citizen read_citizen() {
     //char date[LINE_LENGTH];
     Citizen c;
-    printf("Introduzca el numero de documento: \n");
-    scanf("%d", &c.documento);
-    while (c.documento > 99999999 ){
-        printf("Introduzca el numero de documento: \n");
-        scanf("%d", &c.documento);
+    int correcto;
+    char buffer[200];
+    while(1){
+       printf("Introduzca el numero de documento: \n");
+       if(scanf("%d", &c.documento) != 1){
+           printf("Se ha producido un error\n");
+            scanf("%s", buffer);
+       }
+       else{
+           if(c.documento > 9999999 && c.documento < 99999999){
+                break;
+           }
+           else{
+               printf("El numero de documento debe tener 8 digitos\n");
+           }
+       }
     }
     printf("Introduzca su nombre:\n");
     scanf("%s", c.nombre);
@@ -21,10 +32,6 @@ Citizen read_citizen() {
    printf("Introduzca su fecha de nacimiento(dd/mm/yyyy):\n");
    scanf("%d%*c%d%*c%d", &c.fecha_nacimiento.dia,&c.fecha_nacimiento.mes,&c.fecha_nacimiento.any);
    } while ((c.fecha_nacimiento.dia<0 || c.fecha_nacimiento.dia>30),(c.fecha_nacimiento.mes>12 || c.fecha_nacimiento.mes<0), (c.fecha_nacimiento.any>2017||c.fecha_nacimiento.any<1800));
-     /* do{   
-   printf("Introduzca su fecha de nacimiento(dd/mm/yyyy):\n");
-   scanf("%d%*c%d%*c%d", &c.fecha_nacimiento.dia,&c.fecha_nacimiento.mes,&c.fecha_nacimiento.any);
-   } while ((c.fecha_nacimiento.dia%30+1),(c.fecha_nacimiento.mes%12+1 ), (c.fecha_nacimiento.any%2017+1)); */
     do{
    printf("Introduzca su sexo(M/F):\n");
     scanf(" %c", &c.sexo);
